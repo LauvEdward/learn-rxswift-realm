d3f759d9d4670e60def0fe6065393286eabfa8df
Le Dinh Phuc <phucld@ominext.com>
2024-01-17 17:04:34 +0700

    first commit

---
+++ rxswift_with_realm/rxswift_with_realm/RxSwiftWithRealmApp.swift
@@ -0,0 +1,17 @@
+//
+//  rxswift_with_realmApp.swift
+//  rxswift_with_realm
+//
+//  Created by Mojave on 17/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct RxSwiftWithRealmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
